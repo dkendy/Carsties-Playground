ef8db98cb777f41560549f8080aa188feca8bf12
Davi Yorozuya <davi.kendy@hotmail.com>
2024-11-18 21:13:42 -0300

    Initial with Auction Services

---
+++ src/AuctionService/Data/Migrations/20241117192056_Change fields names.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Changefieldsnames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReservedPrice",
+                table: "Auctions",
+                newName: "ReservePrice");
+
+            migrationBuilder.RenameColumn(
+                name: "ActionEnd",
+                table: "Auctions",
+                newName: "AuctionEnd");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReservePrice",
+                table: "Auctions",
+                newName: "ReservedPrice");
+
+            migrationBuilder.RenameColumn(
+                name: "AuctionEnd",
+                table: "Auctions",
+                newName: "ActionEnd");
+        }
+    }
+}
